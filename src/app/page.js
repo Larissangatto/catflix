6812a58d006ce6e87941a7211a484d954611c4ac
Larissa Nathanna Gatto <larissangatto@gmail.com>
2025-06-12 15:11:15 -0300

    Aplicação do CSS reset

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import MusicsPage from "./musics/page";
 import styles from "./page.module.css";
+import PodcastsPage from "./podcasts/page";
+import VideosPage from "./videos/page";
 
 export default function Home() {
   return (
-    <h2>Ínicio</h2>
+    <>
+      <h2>Ínicio</h2>
+      <VideosPage/>
+      <MusicsPage/>
+      <PodcastsPage/>
+    </>
   )
 }
