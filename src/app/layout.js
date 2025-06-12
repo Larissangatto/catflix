74cc7ea8c86af44dbad26ca91d07c98c0d4c3498
Larissa Nathanna Gatto <larissangatto@gmail.com>
2025-06-12 11:31:31 -0300

    Estruturação do html inicial da aplicação

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt">
       <body>
-        {children}
+        <header>
+          <h1>Catflix</h1>
+          <nav>
+            <a href="/">Home</a>
+            <a href="/videos">Vídeos</a>
+            <a href="/musics">Músicas</a>
+            <a href="/podcasts">Podcasts</a>
+          </nav>
+        </header>
+        <main>
+          {children}
+        </main>
+        <footer>
+          <p>&copy; 2025 Catflix - Larissa Nathanna Gatto</p>
+
+        </footer>
       </body>
     </html>
   )
