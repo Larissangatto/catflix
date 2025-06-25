d09b9dfba865c29990481e842c305b4b7c4031d4
Larissa Nathanna Gatto <larissangatto@gmail.com>
2025-06-25 14:34:09 -0300

    Troca da tag <img/> para <Image/> from 'next/image'

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import styles from './not-found.module.css'
 export default function NotFoundPage(){
     return(
         <div className={styles.notFound}>
             <h2> 404 | Página não encontrada </h2>
-            <img src='/notfound.png'/>
+            <Image src='/notfound.png' width={1200} height={600} alt='Cachorros com olhos arregalados olhando para a página não encontrada'/>
 
         </div>
     )
